b56ca501beb79294e0d7956bfcac4110c4d48d25
FlawlessCasual17 <07e5297d5b@c0x0.com>
2025-04-27 17:33:35 -0600

    Update `rename-my-tv-series/update-script.fish`
    Signed-off-by: FlawlessCasual17 <07e5297d5b@c0x0.com>

--- rename-my-tv-series/update-script.fish
+++ rename-my-tv-series/update-script.fish
@@ -1,14 +1,15 @@
+set -l SCRIPT_DIR $(status dirname)
+
 set -l URL 'https://www.tweaking4all.com/home-theatre/rename-my-tv-series-v2/'
 set -l REGEX 'RenameMyTVSeries-([\\d.]+)-Linux64bit\\.tar\\.gz'
+set -l VERSION $(eval "$SCRIPT_DIR/../scripts/scrape_website" $URL $REGEX 'a.btn-ok' 'href')
 
-set -l VERSION $(../scripts/scrape_website $URL $REGEX 'a.btn-ok' 'href')
-
-cp ./rename-my-tv-series.desktop ./rename-my-tv-series.desktop.bak
+cp "$SCRIPT_DIR/rename-my-tv-series.desktop" "$SCRIPT_DIR/rename-my-tv-series.desktop.bak"
 
 # @fish-lsp-disable-next-line 2001
-set -l CAPTURED_VERSION $(sd 'Version:\\s+([\\d.]+)' '$1' ./rename-my-tv-series.spec.bak)
+set -l CAPTURED_VERSION $(sd 'Version:\\s+([\\d.]+)' '$1' "$SCRIPT_DIR/rename-my-tv-series.desktop.bak")
 
 if test $CAPTURED_VERSION != $VERSION
-  sd "Version:\\s+$CAPTURED_VERSION" "Version:\\s+$VERSION" ./rename-my-tv-series.spec
-  rm ./rename-my-tv-series.desktop.bak
+  sd "Version:\\s+$CAPTURED_VERSION" "Version:\\s+$VERSION" "$SCRIPT_DIR/rename-my-tv-series.desktop"
+  rm "$SCRIPT_DIR/rename-my-tv-series.desktop.bak"
 end
