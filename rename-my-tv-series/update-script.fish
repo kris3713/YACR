ba5816798c6c5a63c24c24c4dedb5bc6a64c7cdb
FlawlessCasual17 <07e5297d5b@c0x0.com>
2025-04-27 18:13:52 -0600

    Update `.github/workflows/update-packages.yml`
    Signed-off-by: FlawlessCasual17 <07e5297d5b@c0x0.com>

--- rename-my-tv-series/update-script.fish
+++ rename-my-tv-series/update-script.fish
@@ -4,12 +4,14 @@ set -l URL 'https://www.tweaking4all.com/home-theatre/rename-my-tv-series-v2/'
 set -l REGEX 'RenameMyTVSeries-([\\d.]+)-Linux64bit\\.tar\\.gz'
 set -l VERSION $("$SCRIPT_DIR/../scripts/scrape_website" $URL $REGEX 'a.btn-ok' 'href')
 
+echo "Backing up `rename-my-tv-series.desktop`"
 cp "$SCRIPT_DIR/rename-my-tv-series.desktop" "$SCRIPT_DIR/rename-my-tv-series.desktop.bak"
 
 # @fish-lsp-disable-next-line 2001
 set -l CAPTURED_VERSION $(sd 'Version:\\s+([\\d.]+)' '$1' "$SCRIPT_DIR/rename-my-tv-series.desktop.bak")
 
-if test $CAPTURED_VERSION != $VERSION
+if test "$CAPTURED_VERSION" != "$VERSION"
   sd "Version:\\s+$CAPTURED_VERSION" "Version:\\s+$VERSION" "$SCRIPT_DIR/rename-my-tv-series.desktop"
-  rm "$SCRIPT_DIR/rename-my-tv-series.desktop.bak"
 end
+
+rm "$SCRIPT_DIR/rename-my-tv-series.desktop.bak"
