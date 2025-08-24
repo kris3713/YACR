3364402641b0bcec0f16f182ae9ab0cb6a1779ab
kris3713 <07e5297d5b@c0x0.com>
2025-08-24 15:42:44 -0600

    Update `scripts/update-package-versions.sh`
    Signed-off-by: kris3713 <07e5297d5b@c0x0.com>

--- scripts/update-package-versions.sh
+++ scripts/update-package-versions.sh
@@ -1,8 +1,28 @@
 echo '----- Installing Python packages -----'
-echo '> pip install uv'
+
+if ! command -v pip &> /dev/null; then
+  #shellcheck disable=SC2016
+  echo '`pip` not found, please install it first'
+  exit 1
+elif ! command -v python &> /dev/null; then
+  #shellcheck disable=SC2016
+  echo '`python` not found, please install it first'
+  exit 1
+fi
+
+# The reason for installing uv from pip is
+# because it provides the latest version, unlike nixpkgs
+echo '> pip install uv --user'
 pip install uv --user
 echo '> cd ./scripts'
 cd ./scripts
+
+if ! command -v uv &> /dev/null; then
+  #shellcheck disable=SC2016
+  echo '`uv` not found, please install it first'
+  exit 1
+fi
+
 echo '> uv venv'
 uv venv
 echo '> source ./.venv/bin/activate'
@@ -13,6 +33,14 @@ echo '> cd ..'
 cd ..
 
 echo '----- Updating package versions -----'
+
+for i in 'fd' 'sd' 'rg' 'fish'; do
+  if ! command -v $i &> /dev/null; then
+    #shellcheck disable=SC2016
+    echo "\`$i\` not found, please install it first"
+    exit 1
+  fi
+done
 
 for i in $(fd 'update-script' .); do
   echo '---------------------------------------'
