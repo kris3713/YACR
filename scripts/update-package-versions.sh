0abec73fb11cb9b359d45036acdae40ce5ce56cf
kris3713 <07e5297d5b@c0x0.com>
2025-09-03 21:56:22 -0600

    Update `scripts/update-package-versions.sh`
    Signed-off-by: kris3713 <07e5297d5b@c0x0.com>

--- scripts/update-package-versions.sh
+++ scripts/update-package-versions.sh
@@ -1,41 +1,45 @@
 set -e
 
-echo '----- Installing Python packages -----'
+BOLD=$(tput bold)
+RESET=$(tput sgr0)
+RED=$(tput setaf 1)
+YELLOW=$(tput setaf 3)
 
 if ! command -v pip &> /dev/null; then
-  #shellcheck disable=SC2016
-  echo '`pip` not found, please install it first'
+  echo "${BOLD}${RED}ERROR: ${RESET}${RED}\`pip\` not found, please install it first$RESET"
   exit 1
 elif ! command -v python &> /dev/null; then
-  #shellcheck disable=SC2016
-  echo '`python` not found, please install it first'
+  echo "${BOLD}${RED}ERROR: ${RESET}${RED}\`python\` not found, please install it first$RESET"
   exit 1
 fi
 
-# The reason for installing uv from pip is
-# because it provides the latest version, unlike nixpkgs
+# The reason for installing uv from pip is because
+# it provides the latest version, unlike nixpkgs
 if ! command -v uv &> /dev/null; then
   echo '> pip install uv --user'
   pip install uv --user
 fi
 
-echo '> cd ./scripts'
-cd ./scripts
-echo '> uv venv'
-uv venv
-echo '> source ./.venv/bin/activate'
-source ./.venv/bin/activate
-echo '> uv pip install -r ./requirements.txt'
-uv pip install -r ./requirements.txt
-echo '> cd ..'
-cd ..
+if [ ! -d './scripts/.venv' ]; then
+  echo "${BOLD}${YELLOW}Installing Python packages...$RESET"
+  echo '> cd ./scripts'
+  cd ./scripts
+  echo '> uv venv'
+  uv venv
+  echo '> source ./.venv/bin/activate'
+  source ./.venv/bin/activate
+  echo '> uv pip install -r ./requirements.txt'
+  uv pip install -r ./requirements.txt
+  echo '> cd ..'
+  cd ..
+fi
 
-echo '----- Updating package versions -----'
+echo "${BOLD}${YELLOW}Updating package versions...$RESET"
 
 for i in 'fd' 'sd' 'rg' 'fish'; do
   if ! command -v $i &> /dev/null; then
     #shellcheck disable=SC2016
-    echo "\`$i\` not found, please install it first"
+    echo "${BOLD}${RED}ERROR: ${RESET}${RED}\`$i\` not found, please install it first$RESET"
     exit 1
   fi
 done
