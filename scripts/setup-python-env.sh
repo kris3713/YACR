5745a65c4e12cec33af74bfd327cdcea68201443
kris3713 <07e5297d5b@c0x0.com>
2025-11-11 21:04:24 -0700

    Added a new workflow for updating the README.md file in the packages directory.
    Signed-off-by: kris3713 <07e5297d5b@c0x0.com>

---
+++ scripts/setup-python-env.sh
@@ -0,0 +1,38 @@
+set -e
+
+echo -e "${YELLOW}Setting up Python environment...$RESET"
+
+RESET='\033[0m'
+RED='\033[0;31m'
+BOLD_RED='\033[1;31m'
+YELLOW='\033[0;33m'
+BOLD_YELLOW='\033[1;33m'
+
+if ! command -v pip &> /dev/null; then
+  echo -e "${BOLD_RED}ERROR: ${RESET}${RED}\`pip\` not found, please install it first$RESET"
+  exit 1
+elif ! command -v python &> /dev/null; then
+  echo -e "${BOLD_RED}ERROR: ${RESET}${RED}\`python\` not found, please install it first$RESET"
+  exit 1
+fi
+
+# The reason for installing uv from pip is because
+# it provides the latest version, unlike nixpkgs
+if ! command -v uv &> /dev/null; then
+  echo '> pip install uv --user'
+  pip install uv --user
+fi
+
+if [ ! -d './scripts/.venv' ]; then
+  echo -e "${BOLD_YELLOW}Installing Python packages...$RESET"
+  echo '> cd ./scripts'
+  cd ./scripts
+  echo '> uv venv'
+  uv venv
+  echo '> source ./.venv/bin/activate'
+  source ./.venv/bin/activate
+  echo '> uv pip install -r ./requirements.txt'
+  uv pip install -r ./requirements.txt
+  echo '> cd ..'
+  cd ..
+fi
