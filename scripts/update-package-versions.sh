a40a6a8f439bc1f2e738f30e13fb7df4a8878c97
Kris <101912712+kris3713@users.noreply.github.com>
2025-11-25 06:08:06 -0700

    Change echo color for updating package versions

--- scripts/update-package-versions.sh
+++ scripts/update-package-versions.sh
@@ -4,7 +4,9 @@ SCRIPT_DIR=$(dirname "$(readlink -f "$0")")
 
 source "$SCRIPT_DIR/setup-python-env.sh"
 
-echo -e "${BOLD_YELLOW}Updating package versions...$RESET"
+BOLD_GREEN='\\e[1;32m'
+
+echo -e "${BOLD_GREEN}Updating package versions...$RESET"
 
 for i in 'fd' 'sd' 'rg' 'fish'; do
   if ! command -v $i &> /dev/null; then
